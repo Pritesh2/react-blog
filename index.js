e5eee9385fff2843796756dc4edf428f0b0b0ec5
Pritesh_Lappy <pritesh@gmail.com>
2020-08-16 16:46:20 +0530

    move private info into dev.js

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 const express=require('express');
 const app=express();
 
+const bodyParser=require('body-parser');
+
+const {User}=require('./model/user');
+const cookieParser=require('cookie-parser');
+
+const config=require('./config/key');
 const mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://Pritesh:123abc@react-blog.j1zka.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser:true
 }).then(()=>console.log('Database connected')).catch(err=>console.error(err));
 
-app.get('/',(req,res)=>{
-    res.send('Hello World')
-});
 
-//mongodb+srv://Pritesh:<password>@react-blog.j1zka.mongodb.net/<dbname>?retryWrites=true&w=majority
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(bodyParser.json())
+app.use(cookieParser());
+
+app.get('/',(req,res)=>{
+    res.json({"hello ~":"Hi ~~"})
+})
+
+app.post('/api/users/register',(req,res)=>{
+    const user=new User(req.body)
+    //console.log("In post")
+
+    user.save((err,userData)=>{
+        if(err)return res.json({success:false,err})
+
+        return res.status(200).json({
+            success:true
+        })
+
+    })
+    
+})
+
+
 app.listen(5000);
 
 
